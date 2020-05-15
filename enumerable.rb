230211896f132c2a9a4d356164358ad27ded04b0
AdewaleK <pearlkelar1@gmail.com>
2020-05-15 16:20:29 +0100

    ADDS my_any ENUMERABLE METHOD

--- enumerable.rb
+++ enumerable.rb
@@ -1,34 +1,45 @@
 module Enumerable
 
-    def my_each
-      x=0
-      until x > self.length - 1
-        yield(self[x])
-        x += 1
-      end
-      self
+  def my_each
+    x=0
+    until x > self.length - 1
+      yield(self[x])
+      x += 1
     end
+    self
+  end
 
-    def my_each_with_index
-      x=0
-      self.my_each do |name|
-        yield(name, x)
-        x += 1
-      end
-      self
+
+  def my_each_with_index
+    x=0
+    self.my_each do |name|
+      yield(name, x)
+      x += 1
     end
+    self
+  end
 
     
   def my_select
     selected = []
     self.my_each do |n|
     if yield(n)
-    selected << n
+      selected << n
     end
   end
   selected
 end
 
+
+def my_any?
+  result = false
+  self.my_each do |item|
+    result = true if yield(item) 
+  end
+  result 
+end
+
+
     
     
     
@@ -36,5 +47,5 @@ end
 
 end
 
-result = [1,2,3,4,5,6].my_select { |n| n.even? }
+result = [1,7,3,].my_any? { |n| n.even? }
 puts result
